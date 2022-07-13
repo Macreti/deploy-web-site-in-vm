38d3952cc8044596824a625cebc7afb629165312
Ismael Macreti <ismacreti72@gmail.com>
2022-07-13 13:44:06 +0100

    update vagrantfile

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 vagrant ssh server-ubuntu << EOF
-sudo docker run --name web-app-test -p 8080:8080 -d hsongwa/web-app-test:latest
+#sudo docker run --name web-app-test -p 8080:8080 -d hsongwa/web-app-test:latest
+sudo docker run --name name_of_container -p expose_port:port_container -d username/name_of_your_image:tag-name
 
 EOF
 
